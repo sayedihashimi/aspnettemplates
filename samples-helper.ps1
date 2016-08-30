0bf9a1aac37a85ef05c04c0a631c2dbfa93d1746
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2016-08-30 14:39:03 -0700

    script updates

--- samples-helper.ps1
+++ samples-helper.ps1
@@ -1,4 +1,7 @@
-﻿$env:IsDeveloperMachine=$true
+﻿[cmdletbinding()]
+param()
+
+$env:IsDeveloperMachine=$true
 function Get-ScriptDirectory
 {
     $Invocation = (Get-Variable MyInvocation -Scope 1).Value
@@ -78,7 +81,7 @@ function Find-Fwlinks{
         $paths = @()
         $result = @()
 
-        Get-ChildItem $rootPath *.* -Recurse -File | select-string $pattern  | % {
+        Get-ChildItem $rootPath *.* -Recurse -File | Select-string 'http://go.microsoft.com' -SimpleMatch  | % {
             try{
                 $mresult = [regex]::Match($_,$pattern).Captures.Groups[0].Value
                 if(-not ([string]::IsNullOrEmpty($mresult))){
@@ -152,7 +155,7 @@ function Remove-UniqueText{
         [string]$exclude = '.git'
     )
     process{
-        $fwlinkresult = Find-Fwlinks
+        $fwlinkresult = Find-Fwlinks -rootPath $rootPath
 
         if( ($fwlinkresult -eq $null ) -or
             ($fwlinkresult.FWLinks -eq $null) -or
@@ -172,8 +175,71 @@ function Remove-UniqueText{
             $replacements[$fwlink]='fwlink'
         }
 
-        # todo improve by finding fwlink extension:Get-ChildItem .\samples\ *.* -Recurse -File | select-string $pattern | %{ (Get-Item $_.path).Extension}|Select-Object -Unique
         Replace-TextInFolder -folder $rootPath -replacements $replacements -include $include -exclude $exclude
+    }
+}
+
+function Normalize-UserSecrets{
+    [cmdletbinding()]
+    param(
+        [string]$rootPath = ($pwd)
+    )
+    process{
+        $matches = Get-ChildItem $rootPath project.json -Recurse -File |select-string '.*\"userSecretsId\".*'|%{ [regex]::Match($_,'[^:]*\"userSecretsId\".*') }
+        if($matches -ne $null){
+                
+            $secretList = @()
+            foreach($m in $matches){
+                $value = ''
+                try{
+                    $value = ($m.Groups[0].Value)    
+                }
+                catch{ }
+                if(-not ([string]::IsNullOrWhiteSpace($value))){
+                    $secretList += ($m.Groups[0].Value)
+                }
+            }
+
+            $secretList = ($secretList | Select-Object -Unique)
+            $replacements = @{}
+            foreach($sec in $secretList){
+                $replacements[$sec]='"usersecretid":"<removed-to-simplify-diff>",'
+            }
+
+            Replace-TextInFolder -folder $rootPath -replacements $replacements -include project.json
+        }
+    }
+}
+
+function Normalize-ConString{
+    [cmdletbinding()]
+    param(
+        [string]$rootPath = ($pwd)
+    )
+    process{
+        $matches = Get-ChildItem $rootPath appSettings.json -Recurse -File |select-string '.*\"DefaultConnection.*'|%{ [regex]::Match($_,'[^:]*\"DefaultConnection\".*') }
+        if($matches -ne $null){
+                
+            $secretList = @()
+            foreach($m in $matches){
+                $value = ''
+                try{
+                    $value = ($m.Groups[0].Value)    
+                }
+                catch{ }
+                if(-not ([string]::IsNullOrWhiteSpace($value))){
+                    $secretList += ($m.Groups[0].Value)
+                }
+            }
+
+            $secretList = ($secretList | Select-Object -Unique)
+            $replacements = @{}
+            foreach($sec in $secretList){
+                $replacements[$sec]='"DefaultConnection":"<removed-to-simplify-diff>",'
+            }
+
+            Replace-TextInFolder -folder $rootPath -replacements $replacements -include appSettings.json
+        }        
     }
 }
 
@@ -212,8 +278,16 @@ function EnsurePecanWaffleLoaded{
 # this will clean it of guids/fwlinks/etc
 function Prepare-SourceDirectory{
     [cmdletbinding()]
-    param()
+    param(
+        [string]$rootPath = (join-path $pwd 'samples')
+    )
     process{
+        EnsureFileReplacerInstlled
+        Normalize-Guids -rootPath $rootPath
+        Normalize-DevServerPort -rootPath $rootPath
+        Remove-UniqueText -rootPath $rootPath
+        Normalize-UserSecrets -rootPath $rootPath
+        Normalize-ConString -rootPath $rootPath
     }
 }
 
@@ -266,20 +340,147 @@ function Get-Fullpath{
     }
 }
 
-$config = New-Object -TypeName psobject -Property @{
-    SourcePath = ($scriptdir | Get-Fullpath)
-    SamplesPath = (Join-Path $scriptdir 'samples' | Get-Fullpath)
-    TargetSourceRoot = ('C:\temp\templates-temp')
-    TargetPath = ('C:\temp\templates-temp\samples')
-    Basebranch = 'master'
+function GetMarkdownReport{
+    [cmdletbinding()]
+    param()
+    process{
+        [System.IO.DirectoryInfo[]]$dirs = (Get-ChildItem -Path ($config.SamplesPath) -Directory)
+        $baseUrl = $config.BaseCompareUrl
+        for($i = 0;$i -lt ($dirs.Length);$i++){
+            $dir1 = $dirs[$i]
+            "`r`n`r`n## {0}`r`n" -f ($dir1.Name) | Write-Output
+
+            $allLinks = @()
+            for($j = 0;$j -lt ($dirs.Length);$j++){
+                if($i -ne $j){
+                    $dir2 = $dirs[$j]
+                    # https://github.com/sayedihashimi/aspnettemplates/compare/CoreIndAuth...CoreIndAuth-CoreNoAuth
+                    $url1to2 = ('{0}{1}...{1}-{2}' -f $baseUrl,($dir1.Name),($dir2.Name))
+                    $url2to1 = ('{0}{2}...{2}-{1}' -f $baseUrl,($dir1.Name),($dir2.Name))
+
+                    $allLinks += '- [{1} → {2}]({0})' -f $url1to2,($dir1.Name),($dir2.Name)
+                    $allLinks +='- [{2} → {1}]({0})' -f $url2to1,($dir1.Name),($dir2.Name)
+                }
+            }
+
+            $allLinks | Sort-Object | Write-Output
+        }
+    }
 }
+
+
+$sourcePathOnAllFiles = (join-path $scriptdir '..\aspnettemplates-allfiles' | Get-Fullpath)
+$sourPathonMaster = (join-path $scriptdir '..\aspnettemplates-master' | Get-Fullpath)
+
+if(-not (Test-Path $sourcePathOnAllFiles)){
+    throw ('Samples path not found at [{0}]' -f $sourcePathOnAllFiles)
+}
+if(-not (Test-Path $sourPathonMaster)){
+    throw ('Samples path not found at [{0}]' -f $sourPathonMaster)
+}
+
+if($sourcePathOnAllFiles -eq $sourPathonMaster){
+    throw ('both source path cannot be the same [{0}] [{1}]' -f $sourcePathOnAllFiles,$sourPathonMaster)
+}
+
+$config = New-Object -TypeName psobject -Property @{
+    SourcePath = $sourcePathOnAllFiles
+    SamplesPath = (Join-Path $sourcePathOnAllFiles 'samples' | Get-Fullpath)
+    TargetSourceRoot = (Join-Path $sourcePathOnAllFiles 'samples' | Get-Fullpath)
+    # TargetSourceRoot = ('C:\temp\templates-temp')
+    TargetSamplesPath = (Join-Path $sourPathonMaster 'samples' | Get-Fullpath)
+    #TargetSamplesPath = ('C:\temp\templates-temp\samples')
+    Basebranch = 'master'
+    BaseCompareUrl = 'https://github.com/sayedihashimi/aspnettemplates/compare/'
+}
+
 
 
 "starting`r`n" | Write-Output
 InternalImport-NuGetPowershell
+EnsureFileReplacerInstlled
 EnsurePecanWaffleLoaded
 
+$global:compareUrls = @()
+function CreateAllDiffs{
+    [cmdletbinding()]
+    param(
+    )
+    process{
+        try{
+            Push-Location
+            Set-Location ($config.TargetSourceRoot) -ErrorAction Stop
 
+            # switch to master branch and clean up the directory before starting
+            $statusResult = (git status -s)
+            if(-not [string]::IsNullOrWhiteSpace($statusResult)){
+                'It looks like there are pending changes in the directory [{0}]' -f $pwd | Write-Host -ForegroundColor Red
+                'Ensure git status -s returns empty before proceeding' | Write-Host -ForegroundColor Red
+                throw 'error'
+            }
+
+            git checkout allfiles 2>&1
+            Prepare-SourceDirectory -rootPath ($config.SourcePath)
+        
+            # get all directories and process each
+            [string[]]$dirs = ((Get-ChildItem ($config.SamplesPath) -Directory).FullName)
+            for($i = 0; $i -lt $dirs.Length;$i++){
+                # prepare the base branch
+                [System.IO.DirectoryInfo]$dir = (Get-Item ($dirs[$i]))
+                $dirName = $dir.Name
+
+                git checkout ($config.Basebranch) 2>&1
+                git reset --hard 2>&1
+                git clean -f 2>&1
+                git branch -D ($dir.Name) 2>&1
+                git checkout -b ($dir.Name) 2>&1
+
+                CopyFiles -sourcePath ($dir.FullName) -destPath ($config.TargetSamplesPath)
+                git add . --all 2>&1
+                git commit -m ("commit [{0}]:[{1}]" -f $dir.Name, $i) 2>&1
+                if($pushToGithub){
+                    git push origin --delete ($dir.Name) 2>&1
+                    git push -u origin ($dir.Name) 2>&1
+                }
+
+                for($j = 0; $j -lt $dirs.Length;$j++){
+                    if( $i -ne $j){
+                        [System.IO.DirectoryInfo]$dir2 = (Get-Item $dirs[$j])
+                        $branchName = "$($dir.Name)-$($dir2.Name)"
+                        git reset --hard 2>&1
+                        git clean -f 2>&1
+                        git checkout ($dir.Name) 2>&1
+                        git branch -D $branchName 2>&1
+                        git checkout -b $branchName 2>&1
+                        CopyFiles -sourcePath ($dir2.FullName) -destPath ($config.TargetSamplesPath)
+                        git add . --all 2>&1
+                        git commit -m ("commit [{0}]:[{1}]:[{2}]" -f $branchName, $i, $j) 2>&1
+
+                        if($pushToGithub){
+                            git push origin --delete $branchName 2>&1
+                            git push -u origin $branchName 2>&1
+                            $compareUrl = '{0}{1}...{2}' -f ($config.BaseCompareUrl),($dir.Name),$branchName
+                            $global:compareUrls += $compareUrl
+                            
+                            $compareUrl | Write-Host -ForegroundColor Cyan
+                        }
+                    }
+                }
+            }
+        }
+        finally{
+            Pop-Location
+
+            $global:compareUrls | clip
+            'Compare urls are on the clipboard' | Write-Host -ForegroundColor Cyan
+        }
+    }
+}
+
+[bool]$pushToGithub = $true
+CreateAllDiffs
+
+<#
 try{
     Push-Location
     Set-Location ($config.TargetSourceRoot) -ErrorAction Stop
@@ -291,7 +492,8 @@ try{
         throw 'error'
     }
 
-    Prepare-SourceDirectory
+    Prepare-SourceDirectory -rootPath ($config.SourcePath)
+
     # switch to the correct branch
     git checkout ($config.Basebranch) | Write-Output
     # clean the folder
@@ -303,7 +505,7 @@ try{
     git branch -D mvcnoauth
     git checkout -b mvcnoauth
     # copy base NoAuth files
-    CopyFiles -sourcePath "$($config.SamplesPath)\MvcNoAuth" -destPath ($config.TargetPath)
+    CopyFiles -sourcePath "$($config.SamplesPath)\MvcNoAuth" -destPath ($config.TargetSamplesPath)
     git add . --all
     git commit -m 'noauth initial'
     
@@ -312,27 +514,42 @@ try{
     if([string]::IsNullOrWhiteSpace($noauthcommitid)){ 
         throw ('Unable to determine value for noauthcommitid') 
     }
+    
+    if($pushToGithub){
+        git push origin --delete mvcnoauth
+        git push -u origin mvcnoauth
+    }
         
     # noauth-indauth
     git checkout mvcnoauth
     git branch -D mvcnoauth-indauth
     git checkout -b mvcnoauth-indauth
-    CopyFiles -sourcePath "$($config.SamplesPath)\MvcIndAuth" -destPath ($config.TargetPath)
+    CopyFiles -sourcePath "$($config.SamplesPath)\MvcIndAuth" -destPath ($config.TargetSamplesPath)
     git add . --all
     git commit -m 'indauth'
-
+    
+    if($pushToGithub){
+        git push origin --delete mvcnoauth-indauth
+        git push -u origin mvcnoauth-indauth
+    }
     # noauth-winauth
     git checkout mvcnoauth
     git branch -D mvcnoauth-winauth
     git checkout -b mvcnoauth-winauth
 
-    CopyFiles -sourcePath "$($config.SamplesPath)\MvcWinAuth" -destPath ($config.TargetPath)
+    CopyFiles -sourcePath "$($config.SamplesPath)\MvcWinAuth" -destPath ($config.TargetSamplesPath)
     git add . --all
     git commit -m 'winauth'
+
+    if($pushToGithub){
+        git push origin --delete mvcnoauth-winauth
+        git push -u origin mvcnoauth-winauth
+    }
 }
 finally{
     Pop-Location
 }
+#>
 
 #### mvcnoauth branch
 # switch to master branch
