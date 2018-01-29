96f3a0bd2669840a011ece375159dc317d4798f0
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2018-01-29 14:35:43 -0500

    moving files

---
+++ samples/VSM/VSM-BlankFormsApp/BlankFormsAppPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace BlankFormsApp
+{
+    public partial class BlankFormsAppPage : ContentPage
+    {
+        public BlankFormsAppPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
