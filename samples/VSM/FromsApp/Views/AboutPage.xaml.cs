361ca23aeb67273aa042d9e00dd8153b89cb3f42
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2018-01-29 11:56:39 -0500

    updating gitignore

---
+++ samples/VSM/FromsApp/Views/AboutPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace FromsApp
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
