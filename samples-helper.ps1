0b863f5ea02f2c2be6735db80d4f4ed2c463e3f5
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2018-07-09 15:27:17 -0400

    minor update

--- samples-helper.ps1
+++ samples-helper.ps1
@@ -545,6 +545,6 @@ function CreateAllDiffs{
 
 [bool]$pushToGithub = $true
 CreateAllDiffs
-
+GetMarkdownReport
 # to delete all remote branchess
 # git branch -a|%{ if(!($_.contains('master')) -and !($_.contains('allfiles'))  ){     'git push origin --delete '+ $_}}|%{$_.replace('  remotes/origin/','')}|clip
